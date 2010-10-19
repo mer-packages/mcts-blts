d1e8b30aa02b5d01e78b358274edb54f71aecbeb
Mika Niinistö <mika.niinisto@digia.com>
2010-10-19 14:18:02 +0300

    blts-xvideo: Update pack.sh
    Signed-off-by: Kimmo Myllyvirta <kimmo.myllyvirta@digia.com>
    Signed-off-by: Jussi Saavalainen <jussi.saavalainen@digia.com>

--- mcts-blts/blts-xvideo/pack.sh
+++ mcts-blts/blts-xvideo/pack.sh
@@ -1,53 +1,132 @@
 #!/bin/sh
+#
+# Packaging script for a single BLTS test case.
+#
+# - Builds a tarball out of the sources, using an exclude pattern
+# - Copies the tarball into rpm source dir, unless explicitly blocked by
+#   command line switch
 
-# pack.sh - Packaging script for a BLTS test case
-#
-#  Copyright (C) 2010, Nokia Corporation.
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, version 2.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+package_name=
+package_version=
+exclude_pattern="*.tar.gz"
+additional_dest=
+no_rpm=
+no_build=
+result=
 
-# Using autoconf and automake to package the sources
-if [ -x ./autogen.sh ]; then
-    ./autogen.sh
-    ./configure
+USAGE="Usage: $0 [-n|-d <destination>|-h]\n"
+USAGE="$USAGE\t-n\tDon't build RPM package\n"
+USAGE="$USAGE\t-d\tAdditionally, copy the source package to <destination>\n"
+USAGE="$USAGE\t-h|-?\tThis spam"
+
+# Parse the one command line argument
+while getopts nhd: f; do
+    case $f in
+        n) no_build="yes";;
+        d) additional_dest=$OPTARG;;
+        h | \?) echo $USAGE; exit 1;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+# Parse name and version from .spec file, if there is one
+package_name=`grep "Name:" *.spec | awk '{print $2}'`
+if [ "x$package_name" = "x" ]; then
+    no_rpm="yes"
 else
-    autoreconf --force -i -s
-    ./configure
+    package_version=`grep "Version:" *.spec | awk '{print $2}'`
 fi
 
+# Parse name, version and release from debian/changelog if there was no .spec
+# file
+if [ "x$no_rpm" = "xyes" ]; then
+    package_name=`grep 'blts' debian/changelog | awk '{print $1; exit}'`
+    package_version=`grep 'blts' debian/changelog | awk '{print $2; exit}' | sed -e "s/[()]//g;s/-.*//g"`
+fi
+
+# Check if package name and version have been set
+if [ "x$package_name" = "x" ]; then
+    echo "Error: Couldn't determine package name!\n"
+    exit 1
+fi
+
+if [ "x$package_version" = "x" ]; then
+    echo "Error: Couldn't determine package version!\n"
+    exit 1
+fi
+
+# Try to clean up as much as possible before packing
 if [ -f Makefile ]; then
-    make dist
-else
-    echo "No Makefile! Cannot make a source distribution!"
-    exit -1
+    make clean
+    make distclean
 fi
 
-# Determine the destination dir
-dest=""
+# Clean (quietly) the old archives
+rm -vf $package_name-*.tar.gz
 
-if [ "x$RPM_BUILD_DIR" = "x" ]; then
-    dest="$HOME/rpmbuild/SOURCES"
-else
-    dest="$RPM_BUILD_DIR/SOURCES"
+# Make a temporary sub dir
+mkdir -vp $package_name-$package_version
+
+# This copy will throw a warning, "can't copy into itself"
+cp -va * -t $package_name-$package_version
+
+# Pack the stuff into tarball
+tar --exclude="$exclude_pattern" -cvzf $package_name-$package_version.tar.gz $package_name-$package_version
+
+if [ ! $? = 0 ]; then
+    echo "Error: Failed to create tarball!\n"
+    exit 1
 fi
 
-if [ ! -d $dest ]; then
-    mkdir -p $dest
+# To clean it up
+rm -vrf $package_name-$package_version
+
+# Do the additional copy if necessary
+if [ ! "x$additional_dest" = "x" ]; then
+    cp -v $package_name-$package_version.tar.gz $additional_dest
+
+    if [ ! $? = 0 ]; then
+        echo "Error: Failed to copy the tarball to $additional_dest!"
+        exit 1
+    fi
 fi
 
-# Then, copy all packaged sources to build dir
-cp *.tar.gz $dest
+# Check command line, is rpm packaging blocked
+if [ "x$no_build" = "x" -a "x$no_rpm" = "x" ]; then
 
-# Then, build the stuff
-rpmbuild -ba $* *.spec
+    # Determine the destination dir
+    dest=""
+
+    if [ "x$RPM_BUILD_DIR" = "x" ]; then
+        dest="$HOME/rpmbuild/SOURCES"
+    else
+        dest="$RPM_BUILD_DIR/SOURCES"
+    fi
+
+    if [ ! -d $dest ]; then
+        mkdir -vp $dest
+
+        if [ ! $? = 0 ]; then
+            echo "Error: Failed to create directory $dest\n"
+            exit 1
+        fi
+    fi
+
+    # Then, copy all packaged sources to build dir
+    cp -v $package_name-$package_version.tar.gz $dest
+
+    if [ ! $? = 0 ]; then
+        echo "Error: Failed to copy $package_name-$package_version.tar.gz to $dest!\n"
+        exit 1
+    fi
+
+    # Then, build the stuff
+    rpmbuild -ba $package_name.spec
+
+    if [ ! $? = 0 ]; then
+        echo "Error: Failed to rpmbuild $package_name!\n"
+        exit 1
+    fi
+fi
+
+exit 0
