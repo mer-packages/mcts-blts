e8c3f484b56e55dd0fa15fd9124535ac4be980cb
Mika Niinistö <mika.niinisto@digia.com>
2010-10-26 10:50:02 +0300

    blts-bluetooth: Open sourced packages: pack.sh changes
    Change functionality to by default just build source package release.
    With additional switch -o, one can define destination to copy the source, and
    binary, packages to given location.
    With -r and -d, it is possible to make the script compile RPM and Debian
    packages.
    
    Signed-off-by: Jussi Saavalainen <jussi.saavalainen@digia.com>

--- mcts-blts/blts-bluetooth/pack.sh
+++ mcts-blts/blts-bluetooth/pack.sh
@@ -1,68 +1,84 @@
 #!/bin/sh
-#
+
 # Packaging script for a single BLTS test case.
 #
-# - Builds a tarball out of the sources, using an exclude pattern
-# - Copies the tarball into rpm source dir, unless explicitly blocked by
-#   command line switch
+# - Builds a tarball out of the sources
+# - Builds a RPM package, if so requested
+# - Builds a Debian package, if so requested
+# - With a command line parameter, release dir can be specified, so
+#   resulting package are copied to that destination
 
 package_name=
 package_version=
 exclude_pattern="*.tar.gz"
 additional_dest=
-no_rpm=
-no_build=
+no_rpm="yes"
+no_debian="yes"
 current=`pwd`
 
-USAGE="Usage: $0 [-n|-d <destination>|-h]\n"
-USAGE="$USAGE\t-n\tDon't build RPM package\n"
-USAGE="$USAGE\t-d\tAdditionally, copy the source package to <destination>\n"
-USAGE="$USAGE\t-h|-?\tThis spam"
+# Spam the usage
+print_usage()
+{
+    echo "Usage: $0 [-o <dir>|-r|-d|-h]"
+    echo "\nScript to build a source package release. By default"
+    echo "only sources are packed, no building is done.\n"
+    echo "\t-o\tCopy files (packages) to <dir>"
+    echo "\t-r\tBuild RPM package, fails if there's no .spec"
+    echo "\t-d\tBuild Debian package, fails if there's no debian/control"
+    echo "\t-h|-?\tThis spam"
+}
 
-# Parse the one command line argument
-while getopts nhd: f; do
+# Parse command line arguments
+while getopts no:rdh f; do
     case $f in
-        n) no_build="yes";;
-        d) additional_dest=$OPTARG;;
-        h | \?) echo $USAGE; exit 1;;
+        o) additional_dest=$OPTARG;;
+        r) no_rpm=;;
+        d) no_debian=;;
+        h | \?) print_usage; exit 1;;
     esac
 done
 shift `expr $OPTIND - 1`
 
-# Parse name and version from .spec file, if there is one
-package_name=`grep "Name:" *.spec | awk '{print $2}'`
-if [ "x$package_name" = "x" ]; then
-    no_rpm="yes"
-else
-    package_version=`grep "Version:" *.spec | awk '{print $2}'`
+if [ "x$no_rpm" = "x" ]; then
+    # Parse name and version from .spec file, if there is one
+    package_name=`grep "Name:" *.spec | awk '{print $2}'`
+    if [ ! "x$package_name" = "x" ]; then
+        package_version=`grep "Version:" *.spec | awk '{print $2}'`
+    else
+        # Can't build RPM
+        no_rpm="yes"
+    fi
 fi
 
 # Parse name, version and release from debian/changelog if there was no .spec
 # file
-if [ "x$no_rpm" = "xyes" ]; then
+if [ "x$no_debian" = "x" -o "x$package_name" = "x" ]; then
     package_name=`grep 'blts' debian/changelog | awk '{print $1; exit}'`
     package_version=`grep 'blts' debian/changelog | awk '{print $2; exit}' | sed -e "s/[()]//g;s/-.*//g"`
 fi
 
 # Check if package name and version have been set
 if [ "x$package_name" = "x" ]; then
-    echo "Error: Couldn't determine package name!\n"
+    echo "Error: Couldn't determine package name!"
     exit 1
 fi
 
 if [ "x$package_version" = "x" ]; then
-    echo "Error: Couldn't determine package version!\n"
+    echo "Error: Couldn't determine package version!"
     exit 1
 fi
 
 # Try to clean up as much as possible before packing
 if [ -f Makefile ]; then
-    make clean
-    make distclean
+    make -s clean
+
+    if [ -x debian/rules ]; then
+        fakeroot debian/rules clean
+    fi
 fi
 
 # Clean (quietly) the old archives
-rm -vf $package_name-*.tar.gz
+rm -f $package_name-*.tar.gz
 
 # Make a temporary sub dir
 mkdir -vp /tmp/$package_name-$package_version
@@ -77,12 +93,12 @@ tar --exclude="$exclude_pattern" -cvzf $current/$package_name-$package_version.t
 cd $current
 
 if [ ! $? = 0 ]; then
-    echo "Error: Failed to create tarball!\n"
+    echo "Error: Failed to create tarball!"
     exit 1
 fi
 
 # To clean it up
-rm -vrf /tmp/$package_name-$package_version
+rm -rf /tmp/$package_name-$package_version
 
 # Do the additional copy if necessary
 if [ ! "x$additional_dest" = "x" ]; then
@@ -95,12 +111,13 @@ if [ ! "x$additional_dest" = "x" ]; then
 fi
 
 # Check command line, is rpm packaging blocked
-if [ "x$no_build" = "x" -a "x$no_rpm" = "x" ]; then
+if [ "x$no_rpm" = "x" ]; then
 
     # Determine the destination dir
     dest=""
 
     if [ "x$RPM_BUILD_DIR" = "x" ]; then
+        RPM_BUILD_DIR="$HOME/rpmbuild"
         dest="$HOME/rpmbuild/SOURCES"
     else
         dest="$RPM_BUILD_DIR/SOURCES"
@@ -110,7 +127,7 @@ if [ "x$no_build" = "x" -a "x$no_rpm" = "x" ]; then
         mkdir -vp $dest
 
         if [ ! $? = 0 ]; then
-            echo "Error: Failed to create directory $dest\n"
+            echo "Error: Failed to create directory $dest"
             exit 1
         fi
     fi
@@ -119,7 +136,7 @@ if [ "x$no_build" = "x" -a "x$no_rpm" = "x" ]; then
     cp -v $package_name-$package_version.tar.gz $dest
 
     if [ ! $? = 0 ]; then
-        echo "Error: Failed to copy $package_name-$package_version.tar.gz to $dest!\n"
+        echo "Error: Failed to copy $package_name-$package_version.tar.gz to $dest!"
         exit 1
     fi
 
@@ -127,9 +144,32 @@ if [ "x$no_build" = "x" -a "x$no_rpm" = "x" ]; then
     rpmbuild -ba $package_name.spec
 
     if [ ! $? = 0 ]; then
-        echo "Error: Failed to rpmbuild $package_name!\n"
+        echo "Error: Failed to rpmbuild $package_name!"
         exit 1
     fi
+
+    # Copy the resulting package into the destination give from command line
+    if [ ! "x$additional_dest" = "x" ]; then
+        find $RPM_BUILD_DIR -name "$package_name-$package_version*.rpm" | xargs -I '{}' cp -v '{}' $additional_dest
+    fi
+fi
+
+# Check from command line, is debian packaging to be done
+if [ "x$no_debian" = "x" ]; then
+
+    dpkg-buildpackage -D -rfakeroot
+
+    if [ ?$ = 0 ]; then
+        echo "Failed to build debian package for $package name"
+        exit 1
+    fi
+
+    # Copy the resulting package into the destination give from command line
+    if [ ! "x$additional_dest" = "x" ]; then
+        find .. -name "$package_name*$package_version*.deb" | xargs -I '{}' cp -v '{}' $additional_dest
+        find .. -name "$package_name*$package_version*.dsc" | xargs -I '{}' cp -v '{}' $additional_dest
+    fi
+
 fi
 
 exit 0
