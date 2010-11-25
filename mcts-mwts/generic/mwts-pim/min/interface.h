3617463ce9dd9f3146b1e1294dca2cfa748ff257
arkataja <arto.katajasalo@digia.com>
2010-11-25 14:50:43 +0200

    Correct licence information added to all test asset src/header files. Tests.xml completed for now.

--- mcts-mwts/generic/mwts-pim/min/interface.h
+++ mcts-mwts/generic/mwts-pim/min/interface.h
@@ -1,20 +1,26 @@
 /* interface.h -- Header for MIN's interface
-
-   Copyright (C) 2000-2010, Nokia Corporation.
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, version 2.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+ *
+ * This file is part of MCTS
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Contact: Tommi Toropainen; tommi.toropainen@nokia.com;
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
 
 #ifndef MIN_SCRIPTER_INTERFACE_H
 #define MIN_SCRIPTER_INTERFACE_H
