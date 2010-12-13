015aae182e708c2f943bfa96d5bb3c390679dbc0
Zheng Kui <kui.zheng@intel.com>
2010-12-14 05:53:39 +0800

    mcts-display-tests initial version, Based on Xrandr API

---
+++ mcts-gfx-tests/mcts-display-tests/src/display_backlight_tests.c
@@ -0,0 +1,113 @@
+/* display_rotate_tests.c -- Rotate screen with 0/90/180/270 and reflect with X/Y
+
+   Copyright (C) 2000-2010, Intel Corporation.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Zheng Kui <kui.zheng@intel.com>
+
+*/
+
+#include <unistd.h>
+#include <X11/X.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <X11/Xlibint.h>
+
+#include <X11/extensions/Xrandr.h>
+#include "display_helper.h"
+
+int test_backlight (double execution_time)
+{
+	UNUSED_PARAM (execution_time);
+	int ret = 0;
+	int ver, rev, event_base, error_base;
+	window_struct params;
+//Screen
+	XRRScreenResources *scr_res = NULL;
+	int i;
+//CRTC
+	XRRCrtcInfo *crtc_info = NULL;
+//output
+	XRROutputInfo *output_info = NULL;
+//Property
+	static Atom backlight;
+	XRRPropertyInfo *prop_info = NULL;
+	long value = 0;
+
+	if (create_window (&params, "Test Window"))
+	{
+		ret = -1;
+		goto cleanup;
+	}
+
+	if (!XRRQueryExtension (params.display, &event_base, &error_base))
+	{
+		LOGERR ("XRRQueryExtension failed\n");
+		ret = -1;
+		goto cleanup;
+	}
+
+	if (!XRRQueryVersion (params.display, &ver, &rev))
+	{
+		LOGERR ("XRRQueryVersion failed\n");
+		ret = -1;
+		goto cleanup;
+	}
+
+	LOG ("XRandR Extension version %i.%i\n", ver, rev);
+	XRRSelectInput (params.display, params.root_window,
+		RRScreenChangeNotifyMask | RRCrtcChangeNotifyMask | RROutputChangeNotifyMask
+		| RROutputPropertyNotifyMask);
+	backlight = XInternAtom (params.display, "Backlight", True);
+//Set Backlight
+	scr_res = XRRGetScreenResources (params.display, params.root_window);
+	for (i = 0; i < scr_res->noutput; i++)
+	{
+		LOG ("RROutput ID: 0x%lx\n", *(scr_res->outputs + i));
+		output_info =
+			XRRGetOutputInfo (params.display, scr_res, *(scr_res->outputs + i));
+		LOG ("	Crtc=0x%lx, name=%s, mm_width=%u, mm_height=%u, connection=%d,\n",
+			output_info->crtc, output_info->name, output_info->mm_width,
+			output_info->mm_height, output_info->connection);
+		if (!output_info->connection && output_info->crtc)
+		{
+			prop_info =
+				XRRQueryOutputProperty (params.display, *(scr_res->outputs + i),
+				backlight);
+			if (prop_info->range)
+				LOG ("	BACKLIGHT range: (%d, %d)\n", *(prop_info->values),
+					*(prop_info->values + 1));
+			for (value = 0; value <= *(prop_info->values + 1); value++)
+			{
+				LOG ("		Setting backlight value: %d\n", value);
+				XRRChangeOutputProperty (params.display, *(scr_res->outputs + i),
+					backlight, XA_INTEGER, 32, PropModeReplace, (unsigned char *) &value,
+					1);
+				output_info =
+					XRRGetOutputInfo (params.display, scr_res, *(scr_res->outputs + i));
+				sleep (1);
+			}
+		}
+	}
+
+cleanup:
+	if (scr_res)
+		XRRFreeScreenResources (scr_res);
+	if (crtc_info)
+		XRRFreeCrtcInfo (crtc_info);
+	if (output_info)
+		XRRFreeOutputInfo (output_info);
+	close_window (&params);
+	return ret;
+}
---
+++ mcts-gfx-tests/mcts-display-tests/src/display_helper.c
@@ -0,0 +1,231 @@
+/* display_helper.c -- For help display testing
+
+   Copyright (C) 2000-2010, Nokia Corporation.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <unistd.h>
+#include "display_helper.h"
+
+Display *open_display ()
+{
+	Display *display = NULL;
+
+	display = XOpenDisplay (NULL);
+	if (!display)
+	{
+		/* DISPLAY environment variable may not exist on non-posix-conformant systems */
+		LOG
+			("XOpenDisplay(NULL) failed, DISPLAY environment variable missing? Trying display :0.\n");
+		display = XOpenDisplay (":0");
+		if (!display)
+		{
+			return NULL;
+		}
+	}
+
+	return display;
+}
+
+int close_window (window_struct * params)
+{
+	if (params->display)
+	{
+		if (params->window)
+		{
+			XUnmapWindow (params->display, params->window);
+			XDestroyWindow (params->display, params->window);
+		}
+
+		if (params->gc)
+		{
+			XFreeGC (params->display, params->gc);
+		}
+
+		XFlush (params->display);
+		XCloseDisplay (params->display);
+	}
+
+	memset (params, 0, sizeof (window_struct));
+
+	return 0;
+}
+
+int create_window (window_struct * params, const char *window_name)
+{
+	return create_window_ex (params, window_name, 0, 0);
+}
+
+int create_window_ex (window_struct * params, const char *window_name,
+	int width, int height)
+{
+	XGCValues values;
+
+	memset (params, 0, sizeof (window_struct));
+	/* Open default display */
+	params->display = open_display ();
+	if (!params->display)
+	{
+		LOGERR ("XOpenDisplay failed\n");
+		goto cleanup;
+	}
+
+	params->screen = XDefaultScreen (params->display);
+	params->root_window = RootWindow (params->display, params->screen);
+	if (!params->root_window)
+	{
+		LOGERR ("No root window\n");
+		goto cleanup;
+	}
+
+	params->visual = DefaultVisual (params->display, params->screen);
+
+	if (!XGetWindowAttributes (params->display, params->root_window,
+			&params->root_window_attributes))
+	{
+		LOGERR ("XGetWindowAttributes failed\n");
+		goto cleanup;
+	}
+
+	if (width)
+	{
+		params->width = width;
+	}
+	else
+	{
+		params->width = params->root_window_attributes.width / 4;
+	}
+	if (height)
+	{
+		params->height = height;
+	}
+	else
+	{
+		params->height = params->root_window_attributes.height / 4;
+	}
+	params->window =
+		XCreateSimpleWindow (params->display, params->root_window, 300, 200,
+		params->width, params->height, 0, 0x0, 0x00FFFFFF);
+	if (!params->window)
+	{
+		LOGERR ("XCreateSimpleWindow failed\n");
+		goto cleanup;
+	}
+
+	if (!XGetWindowAttributes (params->display, params->root_window,
+			&params->window_attributes))
+	{
+		LOGERR ("XGetWindowAttributes failed\n");
+		goto cleanup;
+	}
+
+	if (!XMapWindow (params->display, params->window))
+	{
+		LOGERR ("XMapWindow failed\n");
+		goto cleanup;
+	}
+
+	if (!XRaiseWindow (params->display, params->window))
+	{
+		LOGERR ("XRaiseWindow failed\n");
+		goto cleanup;
+	}
+
+	if (!XStoreName (params->display, params->window, window_name))
+	{
+		LOGERR ("XStoreName failed\n");
+		goto cleanup;
+	}
+
+	values.foreground = BlackPixel (params->display, params->screen);
+	params->gc =
+		XCreateGC (params->display, params->window, GCForeground, &values);
+	if (!params->gc)
+	{
+		LOGERR ("XCreateGC failed\n");
+		goto cleanup;
+	}
+
+//  XFlush(params->display);
+
+	/* Slow down execution a bit. Before test starts user sees a blank, white,
+	   window before any drawing or other activity. This gives some time for user to
+	   react. */
+//  sleep(1);
+
+	return 0;
+
+cleanup:
+	close_window (params);
+	return -1;
+}
+
+int create_simple_window (double execution_time)
+{
+	int ret = 0;
+	int x, y, speedx, speedy;
+	const char *text = "Hello World";
+	window_struct params;
+
+	if (create_window (&params, "Test Window"))
+	{
+		ret = -1;
+		goto cleanup;
+	}
+
+	LOG ("Display width: %d height: %d depth: %d\n", DisplayWidth (params.display,
+			params.screen), DisplayHeight (params.display, params.screen),
+		DefaultDepth (params.display, params.screen));
+
+	timing_start ();
+
+	x = 1;
+	y = 1;
+	speedx = 3;
+	speedy = 2;
+	while (timing_elapsed () < execution_time)
+	{
+		XClearWindow (params.display, params.window);
+		XDrawString (params.display, params.window, params.gc, x, y, text,
+			strlen (text));
+		XDrawRectangle (params.display, params.window, params.gc, 5, 5,
+			params.window_attributes.width - 10,
+			params.window_attributes.height - 10);
+		XFlush (params.display);
+		usleep (10000);
+
+		if (x <= 0 || x >= params.window_attributes.width)
+		{
+			speedx = -speedx;
+		}
+
+		if (y <= 0 || y >= params.window_attributes.height)
+		{
+			speedy = -speedy;
+		}
+
+		x += speedx;
+		y += speedy;
+	}
+
+	timing_stop ();
+
+cleanup:
+	close_window (&params);
+	return ret;
+}
