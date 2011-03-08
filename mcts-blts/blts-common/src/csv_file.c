df62f71ee53f2a1d4860d2589df4d69ca326f403
Jussi Saavalainen <jussi.saavalainen@digia.com>
2011-03-08 15:07:21 +0200

    blts-common: Add extended reporting related files to build
    Signed-off-by: Jussi Saavalainen <jussi.saavalainen@digia.com>

---
+++ mcts-blts/blts-common/src/csv_file.c
@@ -0,0 +1,19 @@
+/* csv_file.c -- CSV file handling (library internal use)
+
+   Copyright (C) 2000-2011, Nokia Corporation.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "csv_file.h"
