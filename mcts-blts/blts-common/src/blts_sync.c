18ac9abb5ccb7ca0e07750d128b470f5a35c49ee
Jussi Saavalainen <jussi.saavalainen@digia.com>
2011-03-28 16:26:36 +0300

    blts-common: Implement named barriers
    Use like generic, except:
      - call blts_sync_add_tag("tagname") from each process to sync
      - use blts_sync_tagged("tagname") from each.
    
    Generic barrier uses slot 0 (named "default").
    
    Signed-off-by: Jussi Saavalainen <jussi.saavalainen@digia.com>

--- mcts-blts/blts-common/src/blts_sync.c
+++ mcts-blts/blts-common/src/blts_sync.c
@@ -23,21 +23,36 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "blts_sync.h"
 #include "blts_log.h"
 
 #define SYNC_SHM_NAME "/blts_sync_shared"
 
+#define SYNC_TAG_LEN 16
+#define SYNC_TAG_COUNT 64
+
+/** Data for each barrier. */
+struct tagged_barrier_entry {
+	sem_t mutex;
+
+	unsigned refcount;
+	unsigned active;
+
+	sem_t barrier_arrive_signal;
+	sem_t barrier_depart_signal;
+
+	char tag[SYNC_TAG_LEN];
+};
 
 /** This will live in /dev/shm/SYNC_SHM_NAME */
 struct shared_data {
 	sem_t sync_mutex;              /**< Mutex for this structure */
-	sem_t barrier_signal;          /**< Barrier arrival */
-	sem_t barrier_continue_signal; /**< Barrier departure */
-	unsigned barrier_active;       /**< Flag: first process to reach barrier sets & manages */
-	unsigned client_count;         /**< Set in -syncprep, needed by barrier */
 	sem_t completion_signal;       /**< Final cleanup signaling */
+
+	struct tagged_barrier_entry barriers[SYNC_TAG_COUNT]; /**< Barriers; 0 = anon, others as reserved by clients. */
+
 } __attribute__((packed));
 
 static struct shared_data *mapped_shm;
@@ -45,6 +60,8 @@ static struct shared_data *mapped_shm;
 static int shm_fd = -1;
 static pid_t pid;
 static unsigned init_done;   /**< Flag: process has sync set up (0 -> API calls are no-ops) */
+
+
 
 static void sync_mutex_lock()
 {
@@ -58,16 +75,6 @@ static void sync_mutex_lock()
 	}
 	/* sem_getvalue(&mapped_shm->sync_mutex, &curr); */
 	/* BLTS_TRACE("[%d] >mutex sem post_lock -> %d\n",pid,curr); */
-}
-
-static int sync_mutex_trylock()
-{
-	return -1;
-}
-
-static int sync_mutex_timedlock(long msec)
-{
-	return -1;
 }
 
 static void sync_mutex_unlock()
@@ -84,33 +91,210 @@ static void sync_mutex_unlock()
 	/* BLTS_TRACE("[%d] >mutex sem post_unlock -> %d\n",pid,curr); */
 }
 
-/* static unsigned tag_count(__attribute__((unused)) char *tag) */
-/* { */
-/* 	return 0; */
-/* } */
+static void barrier_mutex_lock(struct tagged_barrier_entry *b)
+{
+	int err;
+	err = sem_wait(&b->mutex);
+	if(err && errno != EINTR) {
+		BLTS_LOGGED_PERROR("Sync: ERROR while locking barrier mutex");
+	}
+}
+static void barrier_mutex_unlock(struct tagged_barrier_entry *b)
+{
+	int err;
+	err = sem_post(&b->mutex);
+	if(err) {
+		BLTS_LOGGED_PERROR("Sync: ERROR while unlocking barrier mutex");
+	}
+}
 
-int blts_sync_add_tag(__attribute__((unused)) char *tag)
+static int barrier_init(unsigned index, unsigned client_count, char *tag)
+{
+	int err;
+	struct tagged_barrier_entry *b;
+	BLTS_TRACE("[%d] Sync: barrier_init()\n", pid);
+	b = &mapped_shm->barriers[index];
+	if(b->refcount) {
+		BLTS_ERROR("Sync: ERROR: Attempted to init barrier with refcount == 0\n");
+		return -1;
+	}
+
+	memset(b, 0, sizeof(*b));
+
+	err = sem_init(&b->mutex, 1, 0);
+	if(err) {
+		BLTS_LOGGED_PERROR("Sync: ERROR: Can't init barrier mutex");
+		goto err_out;
+	}
+	err = sem_init(&b->barrier_arrive_signal, 1, 0);
+	if(err) {
+		BLTS_LOGGED_PERROR("Sync: ERROR: Can't init barrier arrival semaphore");
+		goto err_out;
+	}
+	err = sem_init(&b->barrier_depart_signal, 1, 0);
+	if(err) {
+		BLTS_LOGGED_PERROR("Sync: ERROR: Can't init barrier departure semaphore");
+		goto err_out;
+	}
+
+	b->active = 0;
+	b->refcount = client_count;
+
+	strncpy(b->tag, tag, SYNC_TAG_LEN);
+	barrier_mutex_unlock(b);
+	return 0;
+
+err_out:
+	b->refcount = 0;
+	return err;
+}
+
+/** Block until all referrers reach barrier. Call locked! */
+static void barrier_sync(unsigned barrier_index)
+{
+	int i;
+	struct tagged_barrier_entry *b;
+
+	b = &mapped_shm->barriers[barrier_index];
+	BLTS_TRACE("[%d] >>> arrived at barrier\n", pid);
+	barrier_mutex_lock(b);
+
+	if(!b->active) {
+		b->active = 1;
+		i = b->refcount - 1;
+		barrier_mutex_unlock(b);
+		sync_mutex_unlock();
+
+		while(i--) {
+			/* BLTS_TRACE("[%d] barrier: 1st waiting others - %d\n",pid, i); */
+			sem_wait(&b->barrier_arrive_signal);
+		}
+		barrier_mutex_lock(b);
+		i = b->refcount - 1;
+		while(i--) {
+			/* BLTS_TRACE("[%d] barrier: 1st signalling others - %d\n",pid, i); */
+			sem_post(&b->barrier_depart_signal);
+
+		}
+		b->active = 0;
+		barrier_mutex_unlock(b);
+		/* BLTS_TRACE("[%d] barrier: 1st done\n",pid); */
+	} else {
+		barrier_mutex_unlock(b);
+		sync_mutex_unlock();
+		/* BLTS_TRACE("[%d] barrier: other signalling 1st\n", pid); */
+		sem_post(&b->barrier_arrive_signal);
+
+		/* BLTS_TRACE("[%d] barrier: other waiting signal\n",pid); */
+		sem_wait(&b->barrier_depart_signal);
+
+		/* BLTS_TRACE("[%d] barrier: other done\n",pid); */
+	}
+	BLTS_TRACE("[%d] <<< leaving barrier\n", pid);
+	sync_mutex_lock();
+}
+
+/** Return index of barrier with given tag; <0 if none. Call locked. */
+static int find_tagged_barrier(char *tag)
+{
+	int i, len;
+	len = strlen(tag);
+	if(len > SYNC_TAG_LEN)
+		len = SYNC_TAG_LEN;
+	for(i = 0; i < SYNC_TAG_COUNT; ++i)
+		if(mapped_shm->barriers[i].refcount &&
+		   !strncmp(mapped_shm->barriers[i].tag, tag, len))
+			return i;
+	return -1;
+}
+
+/**
+ * Insert new tagged barrier, or increment refcount of existing one.
+ * return index to it or <0 if failed. Call locked.
+ */
+static int tagged_barrier_ref(char *tag)
+{
+	int i, err;
+	i = find_tagged_barrier(tag);
+	if(i >= 0) {
+		barrier_mutex_lock(&mapped_shm->barriers[i]);
+		mapped_shm->barriers[i].refcount++;
+		BLTS_TRACE("Sync: '%s'.refcount == %u\n", tag, mapped_shm->barriers[i].refcount);
+		barrier_mutex_unlock(&mapped_shm->barriers[i]);
+		return i;
+	}
+	for(i=0; i < SYNC_TAG_COUNT; ++i)
+		if(!mapped_shm->barriers[i].refcount) {
+			BLTS_TRACE("Sync: '%s'.refcount == 1 (creating)\n", tag);
+			err = barrier_init(i, 1, tag);
+			if(err)
+				return err;
+			return i;
+		}
+	BLTS_ERROR("Sync: Tag table full, cannot add another.\n");
+	return -ENOSPC;
+}
+
+/**
+ * Decrement refcount of tagged barrier, free it if if it reaches zero. Call locked.
+ */
+static void tagged_barrier_unref(char *tag)
+{
+	int i;
+	struct tagged_barrier_entry *b;
+	i = find_tagged_barrier(tag);
+	if(i < 0)
+		return;
+	b = &mapped_shm->barriers[i];
+
+	if(!--b->refcount) {
+		BLTS_TRACE("Sync: Destroying '%s'.\n", tag);
+		sem_destroy(&b->mutex);
+		sem_destroy(&b->barrier_arrive_signal);
+		sem_destroy(&b->barrier_depart_signal);
+		memset(b, 0, sizeof(*b));
+	}
+	BLTS_TRACE("Sync: '%s'.refcount == %u\n", tag, mapped_shm->barriers[i].refcount);
+}
+
+int blts_sync_add_tag(char *tag)
+{
+	int ret;
+	if(!init_done)
+		return 0;
+
+	sync_mutex_lock();
+	ret = tagged_barrier_ref(tag);
+	sync_mutex_unlock();
+	return ret;
+}
+
+int blts_sync_del_tag(char *tag)
 {
 	if(!init_done)
 		return 0;
-	BLTS_DEBUG("WARNING: %s does not do anything yet\n", __FUNCTION__);
+	sync_mutex_lock();
+	tagged_barrier_unref(tag);
+	sync_mutex_unlock();
 	return 0;
 }
 
-int blts_sync_del_tag(__attribute__((unused)) char *tag)
+int blts_sync_tagged(char *tag)
 {
+	int i, ret = 0;
 	if(!init_done)
 		return 0;
-	BLTS_DEBUG("WARNING: %s does not do anything yet\n", __FUNCTION__);
-	return 0;
-}
-
-int blts_sync_tagged(__attribute__((unused)) char *tag)
-{
-	if(!init_done)
-		return 0;
-	BLTS_DEBUG("WARNING: %s does not do anything yet\n", __FUNCTION__);
-	return 0;
+	sync_mutex_lock();
+	i = find_tagged_barrier(tag);
+	if(i < 0) {
+		BLTS_ERROR("Sync: Named barrier '%s' not available\n", tag);
+		ret = -EINVAL;
+	} else {
+		BLTS_TRACE("Sync: Arriving at named barrier '%s'\n", tag);
+		barrier_sync(i);
+	}
+	sync_mutex_unlock();
+	return ret;
 }
 
 int blts_sync_tagged_to(__attribute__((unused)) char *tag, __attribute__((unused)) unsigned long msec)
@@ -121,7 +305,7 @@ int blts_sync_tagged_to(__attribute__((unused)) char *tag, __attribute__((unused
 	return 0;
 }
 
-int blts_sync_anon_to(unsigned long msec)
+int blts_sync_anon_to(__attribute__((unused)) unsigned long msec)
 {
 	if(!init_done)
 		return 0;
@@ -130,46 +314,17 @@ int blts_sync_anon_to(unsigned long msec)
 }
 
 /**
- * Basic barrier.
+ * Default barrier.
  * @return 0
  */
 int blts_sync_anon()
 {
-	unsigned i;
 	if(!init_done)
 		return 0;
-	BLTS_TRACE("[%d] >>> arrived at barrier\n", pid);
+
 	sync_mutex_lock();
-	if(!mapped_shm->barrier_active) {
-		mapped_shm->barrier_active = 1;
-		i = mapped_shm->client_count - 1;
-		sync_mutex_unlock();
-		while(i--) {
-			/* BLTS_TRACE("[%d] barrier: 1st waiting others - %d\n",pid, i); */
-			sem_wait(&mapped_shm->barrier_signal);
-		}
-		sync_mutex_lock();
-		i = mapped_shm->client_count - 1;
-		while(i--) {
-			/* BLTS_TRACE("[%d] barrier: 1st signalling others - %d\n",pid, i); */
-			sem_post(&mapped_shm->barrier_continue_signal);
-
-		}
-		i = mapped_shm->barrier_active = 0;
-		sync_mutex_unlock();
-		/* BLTS_TRACE("[%d] barrier: 1st done\n",pid); */
-	} else {
-		sync_mutex_unlock();
-		/* BLTS_TRACE("[%d] barrier: other signalling 1st\n", pid); */
-		sem_post(&mapped_shm->barrier_signal);
-
-		/* BLTS_TRACE("[%d] barrier: other waiting signal\n",pid); */
-		sem_wait(&mapped_shm->barrier_continue_signal);
-
-		/* BLTS_TRACE("[%d] barrier: other done\n",pid); */
-	}
-	BLTS_TRACE("[%d] <<< leaving barrier\n", pid);
-
+	barrier_sync(0);
+	sync_mutex_unlock();
 	return 0;
 }
 
@@ -312,6 +467,7 @@ int blts_sync_master_init(unsigned client_count)
 	for(i = sysconf(_SC_OPEN_MAX)-1; i >= 3; --i)
 		close(i);
 
+
 	/* Ok, now the actual setup. */
 
 	pid = getpid();
@@ -324,7 +480,7 @@ int blts_sync_master_init(unsigned client_count)
 
 	err = sem_init(&mapped_shm->sync_mutex, 1, 0);
 	if(err) {
-		/* BLTS_LOGGED_PERROR("Sync: ERROR: Can't init sync semaphore"); */
+		/* BLTS_LOGGED_PERROR("Sync: ERROR: Can't init master mutex"); */
 		goto daemon_done;
 	}
 	err = sem_init(&mapped_shm->completion_signal, 1, 0);
@@ -332,19 +488,10 @@ int blts_sync_master_init(unsigned client_count)
 		/* BLTS_LOGGED_PERROR("Sync: ERROR: Can't init completion semaphore"); */
 		goto daemon_done;
 	}
-	err = sem_init(&mapped_shm->barrier_signal, 1, 0);
-	if(err) {
-		/* BLTS_LOGGED_PERROR("Sync: ERROR: Can't init barrier semaphore"); */
-		goto daemon_done;
-	}
-	err = sem_init(&mapped_shm->barrier_continue_signal, 1, 0);
-	if(err) {
-		/* BLTS_LOGGED_PERROR("Sync: ERROR: Can't init barrier semaphore"); */
-		goto daemon_done;
-	}
 
-	mapped_shm->client_count = client_count;
-	mapped_shm->barrier_active = 0;
+	err = barrier_init(0, client_count, "default");
+	if(err)
+		goto daemon_done;
 
 	sync_mutex_unlock();
 
@@ -354,6 +501,19 @@ int blts_sync_master_init(unsigned client_count)
 	}
 	/* BLTS_TRACE("master: %d clients signaled\n", client_count); */
 
+	/* final cleanup */
+
+	sync_mutex_lock();
+	for(i = 0; i < SYNC_TAG_COUNT; ++i) {
+		if(&mapped_shm->barriers[i].refcount) {
+			sem_destroy(&mapped_shm->barriers[i].mutex);
+			sem_destroy(&mapped_shm->barriers[i].barrier_arrive_signal);
+			sem_destroy(&mapped_shm->barriers[i].barrier_depart_signal);
+		}
+	}
+	sem_destroy(&mapped_shm->completion_signal);
+	sync_mutex_unlock();
+	sem_destroy(&mapped_shm->sync_mutex);
 daemon_done:
 	if(mapped_shm)
 		munmap(mapped_shm, sizeof(struct shared_data));
@@ -381,9 +541,10 @@ done:
 
 void blts_sync_client_cleanup()
 {
-	if(mapped_shm)
+	if(mapped_shm) {
 		sem_post(&mapped_shm->completion_signal);
 		munmap(mapped_shm, sizeof(struct shared_data));
-	mapped_shm = NULL;
+		mapped_shm = NULL;
+	}
 }
 
